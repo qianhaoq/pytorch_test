f48a15e1f8632a48c45d03706eeba2f8966078db
qianhaoq <362609254@qq.com>
2017-12-01 21:55:57 +0800

    格式调整

---
+++ chapter-3/logistic_model.py
@@ -0,0 +1,82 @@
+"""
+Logistic 回归
+author: qianhao
+"""
+import torch
+from torch import nn
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+torch.manual_seed(2017)
+
+with open('data.txt', 'r') as f:
+    datalist = f.readlines()
+    datalist = [i.split('\n')[0] for i in datalist]
+    datalist = [i.split(',') for i in datalist]
+    data = [(float(i[0]), float(i[1]), float(i[2])) for i in datalist]
+    #print(data)
+
+x0 = list(filter(lambda x: x[-1] == 0.0, data))
+x1 = list(filter(lambda x: x[-1] == 1.0, data))
+plot_x0_0 = [i[0] for i in x0]
+plot_x0_1 = [i[1] for i in x0]
+plot_x1_0 = [i[0] for i in x1]
+plot_x1_1 = [i[1] for i in x1]
+
+plt.plot(plot_x0_0, plot_x0_1, 'ro', label='x_0')
+plt.plot(plot_x1_0, plot_x1_1, 'bo', label='x_1')
+# loc='best'代表自适应大小
+plt.legend(loc='best')
+
+# transform to tensor
+np_data = np.array(data, dtype=np.float32)
+x_data = torch.from_numpy(np_data[:, 0:2])
+y_data = torch.from_numpy(np_data[:, -1])
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self):
+        super(LogisticRegression, self).__init__()
+        self.lr = nn.Linear(2, 1)
+        self.sm = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.lr(x)
+        x = self.sm(x)
+        return x
+
+if torch.cuda.is_available():
+    logistic_model = LogisticRegression().cuda()
+else:
+    logistic_model = LogisticRegression()
+
+# BCE是2分类问题的损失函数
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(logistic_model.parameters(), lr = 1e-3, momentum=0.9)
+
+# 训练模型
+for epoch in range(50000):
+    if torch.cuda.is_available():
+        x = Variable(x_data).cuda()
+        y = Variable(y_data).cuda()
+    else:
+        x = Variable(x_data)
+        y = Variable(y_data)
+    # forward
+    out = logistic_model(x)
+    loss = criterion(out, y)
+    print_loss = loss.data[0]
+    mask = out.ge(0.5).float()
+    correct = (mask == y).sum()
+    acc = correct.data[0] / x.size(0)
+    # backward
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if (epoch + 1) % 1000 == 0:
+        print('*'*10)
+        print('epoch {}'.format(epoch+1))
+        print('loss is {:.4f}'.format(print_loss))
+        print('acc is {:.4f}'.format(acc))
+

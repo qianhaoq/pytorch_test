301c9d25545249cc19050b04a3f69972510a4476
qianhaoq <362609254@qq.com>
2018-03-28 13:23:15 +0800

    update

--- dist/alexnet.py
+++ dist/alexnet.py
@@ -32,7 +32,7 @@ class AlexNet(nn.Module):
         self.classifier = nn.Sequential(
             nn.Dropout(),
             # nn.Linear(256 * 6 * 6, 4096),
-            nn.Linear(150528, 4096),
+            nn.Linear(12544, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
@@ -42,11 +42,12 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        print(x)
-        print(x.size(0))
-        x = x.view(x.size(0), 256 * 6 * 6)
-        print("as")
-        print(x)
+        # print(x)
+        # print(x.size(0))
+        # x = x.view(x.size(0), 256 * 6 * 6)
+        x = x.view(x.size(0), -1)
+        # print("as")
+        # print(x)
         x = self.classifier(x)
         return x
 
